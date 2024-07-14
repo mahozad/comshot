f625e97aa2735ce5f90fb91ce721a6795b501380
merati22 <merati22@gmail.com>
2024-07-14 14:57:58 +0330

    add skiko-awt-runtime into gradle config for implement skiko for each OS

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -90,7 +90,24 @@ kotlin {
         val desktopMain by getting {
             dependencies {
                 // api(libs.androidx.ui.desktop)
-                // implementation("org.jetbrains.skiko:skiko-awt-runtime-windows-x64:0.7.92")
+                val osName = System.getProperty("os.name")
+                val targetOs = when {
+                    osName == "Mac OS X" -> "macos"
+                    osName.startsWith("Win") -> "windows"
+                    osName.startsWith("Linux") -> "linux"
+                    else -> error("Unsupported OS: $osName")
+                }
+
+                val osArch = System.getProperty("os.arch")
+                val targetArch = when (osArch) {
+                    "x86_64", "amd64" -> "x64"
+                    "aarch64" -> "arm64"
+                    else -> error("Unsupported arch: $osArch")
+                }
+
+                val version = "0.8.9" // or any more recent version
+                val target = "${targetOs}-${targetArch}"
+                implementation("org.jetbrains.skiko:skiko-awt-runtime-$target:$version")
             }
         }
         val desktopTest by getting {
